a2dac3d7c230b3b8efb5ce4ee8bec0999a08cbea
Jian <jianchau@outlook.com>
2020-12-22 22:25:08 +0800

    20201222pm

---
+++ src/pages/employeeFile/DeleteEmployeeFile.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function DeleteEmployeeFile() {
+    return (
+        <div>
+            删除员工档案
+        </div>
+    )
+}
+
+export default DeleteEmployeeFile
---
+++ src/pages/information/LookUpInformation.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function LookUpInformation() {
+    return (
+        <div>
+            查看人才信息
+        </div>
+    )
+}
+
+export default LookUpInformation
